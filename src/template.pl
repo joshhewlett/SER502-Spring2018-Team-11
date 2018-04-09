c8337ed27b22a2b0eae33062ee64497658f0886d
Paul Horton <giansar26@gmail.com>
2018-04-09 16:12:27 -0700

    Added comments to template

--- src/template.pl
+++ src/template.pl
@@ -1,7 +1,13 @@
+% What function to run when initialized
 :- initialization main.
 
 main :-
+	% Get the arguments passed from command line
 	current_prolog_flag(argv,Argv),
+	% This prints out the arguments so it's easier to see what is going on
 	format("Parsing file ~w \n",Argv),
+	% Parsing code goes here
+
+	% Ends the execution
 	halt.
 
