0b07012eba17a329e64a8ac9bb5f687af3ddcd33
Josh Hewlett <jhewlett@asu.edu>
2018-04-09 22:38:45 -0700

    Bash script and tokenizer finished (Aside from token->interpreter)

--- src/compile.sh
+++ src/compile.sh
@@ -1,29 +1,92 @@
 #!/bin/bash
 
+
+# Global variables
+dir='./ahjc/'
+file=''
+betterParsePrefix='betterParse_'
+keepDirFlag=false
+verboseFlag=false
+
+# Get flags/arguments
+while getopts 'kvhd:f:' flag; do
+    case "${flag}" in
+        d) dir="./${OPTARG}/" ;; # Directory argument
+        f) file="${OPTARG}" ;; # File to compile argument
+        k) keepDirFlag=true;; # Keep dump directory flag
+        v) verboseFlag=true;; # Verbose - Log throughout script
+        h) echo ""
+            echo "AH-J Compiler"
+            echo "Team 11"
+            echo ""
+            echo "Options: "
+            echo "-h        Show help"
+            echo "-f        File to compile"
+            echo "-d        Directory to dump generated files"
+            echo "-k=FLAG   Keep generated files"
+            echo "-v=FLAG   Show logs"
+            echo ""
+            exit 0
+            ;;
+        *) error "Unexpected option ${flag}"
+            exit 0
+            ;;
+    esac
+done
+
+# If file not provided, print error and exit
+if [ "$file" = '' ] ; then
+    echo "ERROR: Must provide file to compile"
+    echo "See HELP (-h) for use"
+    exit 0
+fi
+
 ### Create tmp directory and files
 
-betterParsePrefix="betterParse_"
-betterParseFile=$betterParsePrefix$1
-echo $betterParseFile
+# Create new filename for dumping new file to parse
+betterParseFile=$betterParsePrefix$file
+
+# Logs
+if [ "$verboseFlag" = true ] ; then
+    echo ""
+    echo "Tokenizing file: $file"
+    echo "Converting '$file' to '$betterParseFile'"
+    echo "Dumping '$betterParseFile' to '$dir'"
+    echo ""
+fi
 
 # Remove tmp directory if exists
-if [ -f $file ]; then
-    rm -R tmp
+if [ -d $dir ]; then
+    rm -R $dir
 fi
 
 # Create directory and file
-mkdir tmp
-tmp='./tmp/'
+mkdir $dir
 touch $betterParseFile
 
 # Read from argument file and replace all occurences of '.' with ' . '
 while IFS='' read -r line || [[ -n "$line" ]]; do
     string=${line/'.'/' . '}
-    echo $string >> $tmp$betterParseFile
+    echo $string >> $dir$betterParseFile
 
-done < "$1"
+done < "$file"
 
-#Parse
-swipl -s template.pl $tmp$betterParseFile
+# Parse and wait until process is finished
+swipl -s template.pl $dir$betterParseFile $ &
+wait $!
+
+if [ "$verboseFlag" = true ] ; then
+    echo ""
+    echo "Finished creating tokens"
+    echo ""
+fi
 
 #Interpret
+
+
+# If keepDirFlag=true, keep the dump directory.
+# Delete if not.
+if [ $keepDirFlag = false ] ; then
+    rm -R $dir
+    rm ./$betterParseFile
+fi
--- src/template.pl
+++ src/template.pl
@@ -2,32 +2,35 @@
 
 % What function to run when initialized
 :- initialization(main, main).
-:- use_module(library(main)).
 
 % Loaded libraries
 :- use_module(library(pio)).
 
-main(Argv) :-
+main :-
 	% Get the arguments passed from command line
 	current_prolog_flag(argv,[Arg1|_]),
 
-	% This prints out the arguments so it's easier to see what is going on
+	% User logs for clarity
+	write('Tokenizing program...'),
+	write('\n'),
 	format("Parsing file ~w \n",Arg1),
 	% Parsing code goes here
 
-	%atom_codes(Arg1, [H|_]),
-	%atomic_list_concat([H|_], CharCode),
 	phrase_from_file(tokens(Ls), Arg1),
 
-	write('Hello!'),
+	write('\nParsed tokens: '),
+	write(Ls),
 	% Ends the execution
 	halt.
 
+% Create tokens from file
 tokens([]) --> call(eos), !.
 tokens([Token|Tokens]) --> splitToken(Token), tokens(Tokens).
 
+% End of file
 eos([], []).
 
+% Split each token by a space
 splitToken([]) --> ( "\n" ; call(eos) ), !.
 splitToken([]) --> ( " " ; call(eos) ), !.
 splitToken([L|Ls]) --> [L], splitToken(Ls).
