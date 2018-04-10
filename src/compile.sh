b7becff80fc0f08c6e18e0c517bdddaa2254d734
Josh Hewlett <jhewlett@asu.edu>
2018-04-09 21:16:37 -0700

    Working on bash and prolog script for tokenizing

--- src/compile.sh
+++ src/compile.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-#Tokenize
+### Create tmp directory and files
+
+betterParsePrefix="betterParse_"
+betterParseFile=$betterParsePrefix$1
+echo $betterParseFile
+
+# Remove tmp directory if exists
+if [ -f $file ]; then
+    rm -R tmp
+fi
+
+# Create directory and file
+mkdir tmp
+tmp='./tmp/'
+touch $betterParseFile
+
+# Read from argument file and replace all occurences of '.' with ' . '
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    string=${line/'.'/' . '}
+    echo $string >> $tmp$betterParseFile
+
+done < "$1"
 
 #Parse
-swipl -s template.pl $1
+swipl -s template.pl $tmp$betterParseFile
 
 #Interpret
--- src/template.pl
+++ src/template.pl
@@ -1,13 +1,33 @@
-% What function to run when initialized
-:- initialization main.
+#!/usr/bin/env swipl
 
-main :-
+% What function to run when initialized
+:- initialization(main, main).
+:- use_module(library(main)).
+
+% Loaded libraries
+:- use_module(library(pio)).
+
+main(Argv) :-
 	% Get the arguments passed from command line
-	current_prolog_flag(argv,Argv),
+	current_prolog_flag(argv,[Arg1|_]),
+
 	% This prints out the arguments so it's easier to see what is going on
-	format("Parsing file ~w \n",Argv),
+	format("Parsing file ~w \n",Arg1),
 	% Parsing code goes here
 
+	%atom_codes(Arg1, [H|_]),
+	%atomic_list_concat([H|_], CharCode),
+	phrase_from_file(tokens(Ls), Arg1),
+
+	write('Hello!'),
 	% Ends the execution
 	halt.
 
+tokens([]) --> call(eos), !.
+tokens([Token|Tokens]) --> splitToken(Token), tokens(Tokens).
+
+eos([], []).
+
+splitToken([]) --> ( "\n" ; call(eos) ), !.
+splitToken([]) --> ( " " ; call(eos) ), !.
+splitToken([L|Ls]) --> [L], splitToken(Ls).
