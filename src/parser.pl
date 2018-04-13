b098881114a3869c9a6c37bd80c30e86f5fe5383
Alireza <alireza.bahremand@outlook.com>
2018-04-12 21:08:23 -0700

    Fixed Command Bugs, accomodated for list predicate having varied ways of parsing, tested all forms of commands, revised syntax, updated test caes, and sample programs. Updated syntax grammar as well.

--- src/parser.pl
+++ src/parser.pl
@@ -2,14 +2,26 @@ program(t_prog(K)) --> ['Salutations', 'Xiangyu, '],
     list(K),
     ['Sincerely,', 'Ajay', 'Bansal'].
 
-list(t_list(D, C)) --> ['Would', 'you', 'mind', 'doing', 'the', 'following: '],
-    declaration(D), [.], command(C);
-    command(C),
-    ['Thank', 'you.'].
+% This list is for the instance in which the program is declaring variables and 
+% executing commands on them.
+list(t_list(D, C)) --> 
+    ['Would', 'you', 'mind', 'doing', 'the', 'following: '],
+    declaration(D), command(C), ['Thank', 'you.'];
+    declaration(D), block_command(C), ['Thank', 'you.'].
 
+% This list is for the instance in which program is declaring commands with no 
+% declarations.
+list(t_list(C)) --> 
+    ['Would', 'you', 'mind', 'doing', 'the', 'following: '],
+    command(C), ['Thank', 'you.']; 
+    block_command(C), ['Thank', 'you.'].
+
+% Single declaration, TODO: Accomodate for multiple periods from declaration
+% to list predicates.
 declaration(t_decl(I)) -->
     ['Create', 'the', 'variable'], identifier(I), ['.'].
 
+% Multi line declaration of variables.
 declaration(t_decl(I, I2)) -->     
     ['Create', 'the', 'variable'], identifier(I), ['.'],
     [','], declaration(I2).
@@ -28,9 +40,13 @@ command(t_command(I, N)) --> ['Assign', 'the', 'integer'],
     ['to', 'the', 'value', 'of'],
     number(N).
 
+% Should it be the case you have a command followed by
+% another command.
+command(t_command(I, N)) --> command(I), ['.'], command(N).
+
 % Command can be comprised of a while loop, or conditional check.
 % If this fails, we are doing a evaluated assignment.
-command(t_command(N)) --> while_command(N); if_command(N).
+block_command(t_block_cmnd(N)) --> while_command(N); if_command(N).
 
 % Break apart boolean and command, consume syntax to generate tree.
 while_command(t_while(X, Y)) --> ['So', 'long', 'as'],
@@ -47,9 +63,9 @@ if_command(t_if(X, Y, Z)) --> ['Should', 'it', 'be', 'the', 'case'],
     list(Y),
     ['otherwise'],
     list(Z),
-    ['that', 'is', 'all'].
+    ['that', 'is', 'all.'].
 
-
+% Booleans are comprised of evaluating single boolean statements or expressions
 boolean(t_exp(EXP, EXP2)) -->
     exp(EXP),
     ['EQUALS'],
@@ -65,28 +81,32 @@ boolean(t_exp(EXP, EXP2)) -->
 boolean(t_exp(EXP)) --> ['NOT'], exp(EXP).
 boolean(t_exp(EXP)) --> exp(EXP).
 
-
-exp(plus(T,E)) --> term(T),['+'],exp(E).
-exp(minus(T,E)) --> term(T),['-'],exp(E).
-exp(expr(T)) --> term(T).
+% Precedence-defined arithmetic expressions that accomodate
+% order of operations. Also breaks down boolean to TRUE/FALSE 
+% since boolean can consist of expressions, and need a base case.
+exp(t_plus(T,E)) --> term(T),['+'],exp(E).
+exp(t_minus(T,E)) --> term(T),['-'],exp(E).
+exp(t_expr(T)) --> term(T).
 exp(t_bool('TRUE')) --> ['TRUE'].
 exp(t_bool('FALSE')) --> ['FALSE'].
 
-%Precedence
-term(mult(F,T)) --> factor(F),['*'],term(T).
-term(div(F,T)) --> factor(F),['/'],term(T).
-term(term(F)) --> factor(F).
+%Precedence for arithmetic operations.
+term(t_mult(F,T)) --> factor(F),['*'],term(T).
+term(t_div(F,T)) --> factor(F),['/'],term(T).
+term(t_term(F)) --> factor(F).
 
 %Individual values
-factor(factor(I)) --> identifier(I).
-factor(factor(N)) --> number(N).
+factor(t_factor(I)) --> identifier(I).
+factor(t_factor(N)) --> num(N).
 
+% Predicates defining variable names.
+% TODO: Accomodate for multi character strings.
 identifier(t_id(L)) --> letter(L).
 identifier(t_id(I, L)) -->  letter(L), identifier(I).
 
+% TODO: Accomodate for multi digit values.
 num(t_num(L)) --> number(L).
 num(t_num(I, L)) -->  number(I), num(L).
-
 
 letter(a) --> ['a'].
 letter(b) --> ['b'].
