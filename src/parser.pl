37fee2b8d33a85133f91fb4377e07660a5e8ca9c
HalcyonAura <laplace95@gmail.com>
2018-04-17 18:49:30 -0700

    updating grammar and contributions

--- src/parser.pl
+++ src/parser.pl
@@ -2,24 +2,24 @@ program(t_prog(K)) --> ['Salutations', 'Xiangyu,'],
     list(K),
     ['Sincerely,', 'Ajay', 'Bansal'].
 
-% This list is for the instance in which the program is declaring variables and 
+% This list is for the instance in which the program is declaring variables and
 % executing commands on them.
-list(t_list(D, C)) --> 
+list(t_list(D, C)) -->
     ['Would', 'you', 'mind', 'doing', 'the', 'following:'],
     declaration(D), block_command(C), ['Thank', 'you'], ['.'];
     ['Would', 'you', 'mind', 'doing', 'the', 'following:'],
     declaration(D), command(C), ['.'], ['Thank', 'you'], ['.'].
 
-% This list is for the instance in which program is declaring commands with no 
+% This list is for the instance in which program is declaring commands with no
 % declarations.
-list(t_list(C)) --> 
+list(t_list(C)) -->
     ['Would', 'you', 'mind', 'doing', 'the', 'following:'],
     block_command(C), ['.'], ['Thank', 'you'], ['.'];
     ['Would', 'you', 'mind', 'doing', 'the', 'following:'],
     command(C), ['.'], ['Thank', 'you'], ['.'].
 
 % Multi line declaration of variables.
-declaration(t_decl(I, I2)) -->     
+declaration(t_decl(I, I2)) -->
     ['Create', 'the', 'variable'], identifier(I), ['.'], declaration(I2).
 
 % Single declaration, TODO: Accomodate for multiple periods from declaration
@@ -29,7 +29,7 @@ declaration(t_decl(I)) -->
 
 % Assignment of boolean expression expects a identifier var
 % and a boolean value.
-command(t_command(I, B)) --> 
+command(t_command(I, B)) -->
     ['Assign', 'the', 'boolean'],
     identifier(I),
     ['to', 'the', 'value', 'of'],
@@ -41,7 +41,7 @@ command(t_command(I, B)) -->
     command(I), ['.'], block_command(B);
     command(I), ['.'], command(B).
 
-% Integer assignment expects a var identifier along with a 
+% Integer assignment expects a var identifier along with a
 % numerical digit to bind with variable name.
 %command(t_command(I, N)) --> ['Assign', 'the', 'integer'],
 %    identifier(I),
@@ -53,10 +53,18 @@ command(t_command(I, B)) -->
 command(t_command(I, N)) --> command(I), ['.'], command(N);
     command(I), ['.'], block_command(N).
 
+%Print command + assoc. print breakdown
+% command(t_command(V)) --> ['Please', 'reply', 'with',
+% 'the', 'value', 'of'], print(V),['.'].
+%print(t_print(V)) -->
+% identifier(V); boolean(V);number(N).
+
+
+
 % Command can be comprised of a while loop, or conditional check.
 % If this fails, we are doing a evaluated assignment.
-block_command(t_block_cmnd(N, N2)) --> 
-    while_command(N), ['.'], command(N2); 
+block_command(t_block_cmnd(N, N2)) -->
+    while_command(N), ['.'], command(N2);
     if_command(N), ['.'], command(N2).
 % Case where a command proceeds after block command.
 block_command(t_block_cmnd(N)) --> while_command(N); if_command(N).
@@ -78,6 +86,20 @@ if_command(t_if(X, Y, Z)) --> ['Should', 'it', 'be', 'the', 'case'],
     list(Z),
     ['that', 'is', 'all'].
 
+% if, else if, else. TODO: handling multiple else if's
+% if_command(t_if(X, Y, Z, A, B)) --> ['Should', 'it', 'be', 'the',
+% 'case'],
+%    boolean(X),
+%    ['please'],
+%    list(Y),
+%    ['but', 'should', 'it', 'be', 'the', 'case'],
+%    boolean(Z),
+%    ['please'],
+%    list(A),
+%    ['otherwise'],
+%    list(B),
+%    ['that', 'is', 'all'].
+
 % Booleans are comprised of evaluating single boolean statements or expressions
 boolean(t_exp(EXP, EXP2)) -->
     exp(EXP),
@@ -91,11 +113,30 @@ boolean(t_exp(EXP, EXP2)) -->
     exp(EXP),
     ['OR'],
     exp(EXP2).
+
+%Here begins the numerical comparators (<, <=, >, >=)
+%boolean(t_exp(EXP, EXP2)) -->
+%    exp(EXP),
+%    ['LESS','THAN'],
+%    exp(EXP2).
+%boolean(t_exp(EXP, EXP2)) -->
+%    exp(EXP),
+%    ['GREATER','THAN'],
+%    exp(EXP2).
+%boolean(t_exp(EXP, EXP2)) -->
+%    exp(EXP),
+%    ['LESS','THAN','OR','EQUAL','TO'],
+%    exp(EXP2).
+%boolean(t_exp(EXP, EXP2)) -->
+%    exp(EXP),
+%    ['GREATER','THAN','OR','EQUAL','TO'],
+%    exp(EXP2).
+
 boolean(t_exp(EXP)) --> ['NOT'], exp(EXP).
 boolean(t_exp(EXP)) --> exp(EXP).
 
 % Precedence-defined arithmetic expressions that accomodate
-% order of operations. Also breaks down boolean to TRUE/FALSE 
+% order of operations. Also breaks down boolean to TRUE/FALSE
 % since boolean can consist of expressions, and need a base case.
 exp(t_plus(T,E)) --> term(T),['+'],exp(E).
 exp(t_minus(T,E)) --> term(T),['-'],exp(E).
