8ff0f497d3236a27b40cc283bd998fa66d50c0b1
Paul Horton <giansar26@gmail.com>
2018-04-28 13:27:21 -0700

    Got the env to pass properly

--- src/interpreter.pl
+++ src/interpreter.pl
@@ -5,10 +5,12 @@ eval_prog(t_prog(L),Env,FinalEnv) :-
 
 % List evaluations
 eval_list(t_list(D, C),Env,FinalEnv) :-
-    eval_declaration(D,Env,FinalEnv), 
-    eval_block_command(C,Env,FinalEnv);
-    eval_declaration(D,Env,FinalEnv), 
-    eval_command(C,Env,FinalEnv).
+    eval_declaration(D,Env,Env2),
+    eval_block_command(C,Env2,FinalEnv).
+
+eval_list(t_list(D,C),Env,FinalEnv) :-
+    eval_declaration(D,Env,Env2),
+    eval_command(C,Env2,FinalEnv).
 
 eval_list(t_list(D),Env,FinalEnv) :-
     eval_block_command(D,Env,FinalEnv);
@@ -16,12 +18,12 @@ eval_list(t_list(D),Env,FinalEnv) :-
 
 % Decleration evaluations
 eval_declaration(t_decl(I, I2),Env,FinalEnv) :-
-	eval_identifier(I,L),
-    update(L, [], Env, FinalEnv),
-    eval_declaration(I2,Env,FinalEnv).
+    eval_identifier(I,L),
+    update(L, [], Env, Env2),
+    eval_declaration(I2,Env2,FinalEnv).
 
 eval_declaration(t_decl(I),Env,FinalEnv) :-
-	eval_identifier(I, L),
+    eval_identifier(I, L),
     update(L, [], Env, FinalEnv).
 
 % Command evaluations
@@ -29,7 +31,7 @@ eval_command(t_command(I),Env,FinalEnv) :-
     eval_print(I,Env,FinalEnv).
 
 eval_command(t_command(I, I2),Env,FinalEnv) :-
-	eval_identifier(I,L),
+    eval_identifier(I,L),
     eval_boolean(I2,Env,RBool),
     update(L, RBool, Env, FinalEnv);
     eval_identifier(I,L),
@@ -38,54 +40,53 @@ eval_command(t_command(I, I2),Env,FinalEnv) :-
     eval_print(I,Env,FinalEnv), eval_command(I2,Env,FinalEnv).
 
 eval_command(t_command(I, I2, I3),Env,FinalEnv) :-
-	%eval_identifier(I,L),
+    %eval_identifier(I,L),
     %eval_boolean(I2,Env, RBool),
     %update(L, RBool, Env, Env2),
     %eval_block_command(I3,Env2,FinalEnv);
     % alt
-	eval_identifier(I,L),
+    eval_identifier(I,L),
     eval_exp(I2,Env,RExp),
     update(L, RExp, Env, Env2),
     eval_block_command(I3,Env2,FinalEnv);
     % alt
-	eval_identifier(I,L),
+    eval_identifier(I,L),
     eval_boolean(I2,Env,RBool),
     update(L, RBool, Env, Env2),
     eval_command(I3,Env2,FinalEnv);
     % Alt
-	eval_identifier(I,L),
-    eval_exp(I2,Env,RExp), 
+    eval_identifier(I,L),
+    eval_exp(I2,Env,RExp),
     update(L, RExp, Env, Env2),
     eval_command(I3,Env2,FinalEnv).
 
-% Print evaluation shall use prolog 
+% Print evaluation shall use prolog
 % print predicate
 eval_print(t_print(V),_,_) :-
     print(V).
 
 % Block Command Evaluations
 eval_block_command(t_block_cmnd(N),Env,FinalEnv) :-
-	eval_while_command(N,Env,FinalEnv); 
+    eval_while_command(N,Env,FinalEnv);
     eval_if_command(N,Env,FinalEnv).
 
 eval_block_command(t_block_cmnd(N, N2),Env,FinalEnv) :-
-	eval_while_command(N,Env,FinalEnv), 
-    eval_command(N2,Env,FinalEnv);
-    eval_if_command(N,Env,FinalEnv), 
-    eval_command(N2,Env,FinalEnv).
-    
+    eval_while_command(N,Env,Env2),
+    eval_command(N2,Env2,FinalEnv);
+    eval_if_command(N,Env,Env2),
+    eval_command(N2,Env2,FinalEnv).
+
 % While and conditional evaluation,
 % futher breaking down parse tree to commands.
 eval_while_command(t_while(X, Y),Env,FinalEnv) :-
-    eval_boolean(X,Env,FinalEnv),
-    eval_list(Y,Env,FinalEnv).
+    eval_boolean(X,Env,Env2),
+    eval_list(Y,Env2,FinalEnv).
 
 eval_if_command(t_if(X, Y, Z),Env,FinalEnv) :-
-    eval_boolean(X,Env,FinalEnv),
-    eval_list(Y,Env,FinalEnv),
-    eval_list(Z,Env,FinalEnv).
-    
-% == BOOL eval =======================
+    eval_boolean(X,Env,Env2),
+    eval_list(Y,Env2,Env3),
+    eval_list(Z,Env3,FinalEnv).
+
 % == BOOL eval =======================
 eval_boolean(t_exp_eq(EXP, EXP2),Env, Result) :-
     eval_exp(EXP,Env, RExp1),
@@ -129,8 +130,7 @@ bool_or(1,_,1).
 bool_or(_,1,1).
 bool_or(0,0,0).
 
-
-% x + y              
+% x + y
 eval_exp(t_plus(T,E),Env,Result) :-
     eval_term(T,Env,RTerm),
     eval_exp(E,Env,RExp),
@@ -148,12 +148,12 @@ eval_exp(t_bool_false(_),_,0).
 
 %Precedence for arithmetic operations.
 % x * y
-eval_term(t_mult(F,T),Env,Result) :- 
+eval_term(t_mult(F,T),Env,Result) :-
     eval_factor(F,Env,RFactor),
     eval_term(T,Env,RTerm),
     Result is RFactor * RTerm.
 % x / y
-eval_term(t_div(F,T),Env,Result) :- 
+eval_term(t_div(F,T),Env,Result) :-
     eval_factor(F,Env,RFactor),
     eval_term(T,Env,RTerm),
     Result is RFactor / RTerm.
@@ -161,17 +161,17 @@ eval_term(t_div(F,T),Env,Result) :-
 eval_term(t_term(F),Env,Result) :- eval_factor(F,Env,Result).
 
 %Individual values
-eval_factor(t_factor(N),_,Result) :- 
+eval_factor(t_factor(N),_,Result) :-
     eval_num(N,Result).
-eval_factor(t_factor(I),Env,Result) :- 
+eval_factor(t_factor(I),Env,Result) :-
     eval_identifier(I,L),
-    lookup(L, Env, Result).                
+    lookup(L, Env, Result).
 
 eval_identifier(t_id(L),L).
 
 eval_num(t_num(N),Result):-
     atom_number(N,Result).
-    
+
 % Predicate takes a 'x' value to find in Env
 % once matched, returns the value of the
 % identifier variable via tuple pair (Id, Val).
@@ -187,5 +187,5 @@ update(Search, Val, [(Search, _)|Env1], [(Search,Val)|Env1]).
 update(Search, Val, [H|Env1], [H|Env2]) :- update(Search, Val, Env1, Env2).
 
 interpreter(PTokens, FinalEnv) :-
-	eval_prog(PTokens, Output, FinalEnv),
+    eval_prog(PTokens, Output, FinalEnv),
     print(Output), print(FinalEnv).
